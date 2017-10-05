6cf9545309166ff75566b1d6bce4899b37133135
kuzma <kuzma@maill.ru>
2017-10-05 09:04:41 +0300

    first commit

--- exchange.py
+++ exchange.py
@@ -1,8 +1,8 @@
 import MySQLdb
 import datetime
 
-now = datetime.datetime.now() - datetime.timedelta(days=5)
-cutTime = now.strftime("%Y-%m-%d")
+now = datetime.datetime.now() - datetime.timedelta(days=5) # date -1 day
+cutTime = now.strftime("%Y-%m-%d") # format date
 
 db = MySQLdb.connect(host="127.0.0.1", user="root", passwd="170270", db="sint_odessa", charset='utf8')
 cursor = db.cursor()
