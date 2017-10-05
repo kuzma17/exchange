34469751d789119c2bd57905babfad395fe66bcb
kuzma <kuzma@maill.ru>
2017-10-05 08:53:26 +0300

    first commit

---
+++ exchange.py
@@ -0,0 +1,19 @@
+import MySQLdb
+import datetime
+
+now = datetime.datetime.now() - datetime.timedelta(days=5)
+cutTime = now.strftime("%Y-%m-%d")
+
+db = MySQLdb.connect(host="127.0.0.1", user="root", passwd="170270", db="sint_odessa", charset='utf8')
+cursor = db.cursor()
+sql = "SELECT name,email FROM users WHERE created_at >= "+ cutTime
+cursor.execute(sql)
+data = cursor.fetchall()
+
+for rec in data:
+
+    print rec[0] +' '+ rec[1]
+
+db.close()
+
+print cutTime
