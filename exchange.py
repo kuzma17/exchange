01493f981daa77738fd7b23f36af611611add15e
kuzma17 <v.kuzma@mail.ru>
2017-10-06 19:11:39 +0300

    выгрузка клиентов

--- exchange.py
+++ exchange.py
@@ -1,27 +1,19 @@
-# -*- coding: utf-8 -*-
 import MySQLdb
 import csv
-import datetime
 
-#now = datetime.datetime.now() - datetime.timedelta(days=1)
-#cutTime = now.strftime("%Y-%m-%d")
-
-day_ex = 4
-db = MySQLdb.connect(host="127.0.0.1", user="root", passwd="170270", db="sint_odessa", charset='utf8')
+day_ex = 5
+file_csv = '/tmp/file.csv'
+db = MySQLdb.connect(host="127.0.0.1", user="root", passwd="170270", db="sint_odessa", charset='utf8', use_unicode=False)
 cursor = db.cursor()
-#sql = "SELECT name,email,created_at FROM users WHERE created_at >= NOW()-INTERVAL %s DAY" %day_ex
 
 sql = "SELECT users.id,1c_id,type_client_id,type_payment_id,client_name,delivery_town,delivery_street,delivery_house,delivery_house_block,delivery_office,phone,user_company,company_full,edrpou,inn,code_index,region,area,city,street,house,house_block,office,user_profiles.created_at,user_profiles.updated_at,name,email FROM user_profiles LEFT JOIN users ON user_profiles.user_id = users.id WHERE users.updated_at > NOW()-INTERVAL %s DAY OR user_profiles.updated_at > NOW()-INTERVAL %s DAY"
-cursor.execute(sql,(day_ex,day_ex))
-data = cursor.fetchall()
+cursor.execute(sql, (day_ex, day_ex))
+#data = cursor.fetchall()
+db.close()
 
-fp = open('/tmp/file.csv', 'w')
-myFile = csv.writer(fp)
-for rec in data:
-    print rec[4]
-    t = str(rec[2])
-    myFile.writerows('t')
+fp = open(file_csv, 'w')
+myFile = csv.writer(fp, delimiter=';', quotechar='"', quoting=csv.QUOTE_ALL)
+myFile.writerows(cursor)
 fp.close()
 
-db.close()
-#print cutTime
+
